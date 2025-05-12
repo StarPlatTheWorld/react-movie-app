a8a75f5a7c63a4656d076171fd34e318a4b339ee
Ethan McFarlane <117735967+StarPlatTheWorld@users.noreply.github.com>
2025-05-12 17:16:42 +0100

    Initial commit
    Initialised the Git repository within existing directory.

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// Typical Arrow Component Layout
+
+// Arrow Component Functions are easier to read and write, improve code maintainability and simplify scope management.
+
+const Card = () => {
+  return (
+    <div>
+      <h2>Card Component</h2>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+      <h2>Functional Arrow Component</h2>
+
+      <Card />
+      <Card />
+      <Card />
+    </>
+  )
+}
+
+export default App
